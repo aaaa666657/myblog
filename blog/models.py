66080f8c5758fdb9492eee105b75ddf8b5f4275b
KAI <aaaa666657@mail.fcu.edu.tw>
2019-06-10 12:33:01 +0800

    help

--- blog/models.py
+++ blog/models.py
@@ -23,3 +23,18 @@ class Tag(models.Model):
     
     def __str__(self):
         return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=255,verbose_name='活動名稱')
+    users = models.ManyToManyField(User,verbose_name='使用者名單')
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    last_updated = models.DateTimeField(auto_now=True, editable=False)
+    token = models.UUIDField(db_index=True, default=uuid.uuid4)
+
+class Activity(models.Model):
+    name = models.CharField(max_length=255,verbose_name='活動名稱')
+    date = models.DateTimeFiel(verbose_name='活動時間')
+    place = models.CharField(max_length=255,verbose_name='活動地點')
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    last_updated = models.DateTimeField(auto_now=True, editable=False)
+    token = models.UUIDField(db_index=True, default=uuid.uuid4)
